1c087aa7740e779ec9b94dddf1d49a09a1074680
Cameron <cameron-04@hotmail.com>
2024-02-29 12:33:46 -0600

    rename site to chess cage. add logo. remove move bugs txt file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/header.css";
 
+// @ts-ignore
+import logo from "../res/cage.png"
+
 export interface HeaderProps {
 }
 export const Header: React.FC<HeaderProps> = (props) => {
     return <div className="Header">
-        <h1 id="HeaderTitle">Chess Cave</h1>
+        <div className={"Header-Identifiers"}>
+            <img id="HeaderLogo" src={logo} alt={"logo"}/>
+            <h1 id="HeaderTitle">The Cage</h1>
+        </div>
     </div>
 }
